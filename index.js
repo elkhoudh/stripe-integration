2c7c9f5e94a69d7d6ca683f95276dc6df01ec0f9
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-26 14:23:36 -0500

    Uninstalled body parser

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
 
 const server = express();
-
+server.use(express.urlencoded({ extended: false }));
 server.engine("handlebars", exphbs({ defaultLayout: "main" }));
 server.set("view engine", "handlebars");
 server.use(express.static(`${__dirname}/public`));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
+
 server.get("/", (req, res) => {
   res.render("index");
 });
